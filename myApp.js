35bac24957348a4a69d7fd2970316fce89a55a55
gerardojaracarrasco-droid <gerardo.jara.carrasco@gmail.com>
2026-03-12 15:55:42 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,52 @@
 let express = require('express');
 let app = express();
 
+let bodyParser = require('body-parser');
 
+app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
 
+// Logger middleware
+app.use((req,res,next) => {
+  console.log(`${req.method} ${req.path} -  ${req.ip}`);
+  next();
+})
 
+// Homepage
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+})
 
+// JSON endpoint
+app.get("/json", (req, res) => { 
+  if (process.env["MESSAGE_STYLE"] == "uppercase"){
+    res.json({"message": "HELLO JSON"})
+  } else {
+    res.json({"message": "Hello json"})
+  }
+})
 
+// Time middleware
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({"time": req.time});
+})
 
+// Route params
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word});
+})
 
+// Query string
+app.get("/name", (req, res) => {
+  res.json({name: req.query.first + " " + req.query.last});
+})
 
+// POST request
+app.post("/name", (req, res) => {
+  res.json({name: req.body.first + " " + req.body.last});
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
